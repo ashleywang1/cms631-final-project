2e5fb20791c548886ca87510e7bc93bf5ef1fe7f
Ashley Wang <ashleywang1@yahoo.com>
2017-05-10 14:54:35 -0400

    added in numbers for the commute Google API

--- js/commute.js
+++ js/commute.js
@@ -8,9 +8,9 @@ function getCommute(origin, destination, callback) {
         var distance_in_meters = res.rows[0].elements[0].distance.value;
         callback({
             "distance_in_meters": distance_in_meters + " meters!",
-            "car": Math.floor(distance_in_meters*0.001),
-            "bus": Math.floor(distance_in_meters*0.0005),
-            "bike": Math.floor(distance_in_meters*0.00001)
+            "car": Math.floor(distance_in_meters*0.230577999) + " grams", // 371 g/pass-mi
+            "bus": Math.floor(distance_in_meters*0.18582970789)  + " grams", // 299 g/pass-mi
+            "bike": Math.floor(distance_in_meters*0.021) + " grams" //21 g/km
         })
     });
 };
