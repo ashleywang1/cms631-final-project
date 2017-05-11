556a581c2e2c4a9bf20e0db31987bfc5e9b68c6b
Kevin Zhang <kevz@mit.edu>
2017-05-11 15:18:23 -0400

    Check transit

--- js/commute.js
+++ js/commute.js
@@ -3,17 +3,25 @@ function getCommute(origin, destination, callback) {
     service.getDistanceMatrix({
         origins: [origin],
         destinations: [destination],
-        travelMode: 'DRIVING',
+        travelMode: 'TRANSIT',
     }, function (res, status) {
-        var distance_in_meters = res.rows[0].elements[0].distance.value;
-        callback({
-            "distance_in_meters": distance_in_meters,
-            "car": Math.floor(distance_in_meters*0.230577999) + " grams", // 371 g/pass-mi
-            "car_yearly": Math.floor(distance_in_meters*0.230577999*42*5) + " grams",
-            "bus": Math.floor(distance_in_meters*0.18582970789)  + " grams", // 299 g/pass-mi
-            "bike": Math.floor(distance_in_meters*0.021) + " grams", //21 g/km,
-            "pct_saving_if_bus": Math.floor(100*(371-299)/371),
-            "pct_saving_if_bike": Math.floor(100*(371-21)/371),
-        })
-    });
+        var has_transit = res.rows[0].elements[0].status != "ZERO_RESULTS";
+        service.getDistanceMatrix({
+            origins: [origin],
+            destinations: [destination],
+            travelMode: 'DRIVING',
+        }, function (res, status) {
+            var distance_in_meters = res.rows[0].elements[0].distance.value;
+            callback({
+                "distance_in_meters": distance_in_meters,
+                "car": Math.floor(distance_in_meters*0.230577999) + " grams", // 371 g/pass-mi
+                "car_yearly": Math.floor(distance_in_meters*0.230577999*42*5) + " grams",
+                "bus": Math.floor(distance_in_meters*0.18582970789)  + " grams", // 299 g/pass-mi
+                "bike": Math.floor(distance_in_meters*0.021) + " grams", //21 g/km,
+                "pct_saving_if_bus": Math.floor(100*(371-299)/371),
+                "pct_saving_if_bike": Math.floor(100*(371-21)/371),
+                "has_transit": has_transit
+            })
+        });
+    })
 };
