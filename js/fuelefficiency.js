24d55c0acd0ff044b26393f3dc085de7d4b4946f
Kevin Zhang <kevz@mit.edu>
2017-05-15 12:04:12 -0400

    Initial fuel efficiency search

--- js/fuelefficiency.js
+++ js/fuelefficiency.js
@@ -1,5 +1,22 @@
+var fuel;
+$.get("../data/car_data.json", function (car_data) {
+    fuel = new Fuse(car_data, {
+        shouldSort: true,
+        tokenize: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "mfr",
+            "div",
+            "car"
+        ]
+    });
+});
+
 var getFuelEfficiency = function (car_name, callback) {
-    callback({
-        miles_per_gallon: Math.floor(Math.random() * 30)
-    })
+    var result = fuel.search(car_name);
+    callback(result[0]);
 };
