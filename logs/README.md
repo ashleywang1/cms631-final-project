2352add73edf008eaba8ad3ecf2d86ef57ddf975
Kevin Zhang <kevz@mit.edu>
2017-05-17 17:26:52 -0400

    Better stats

--- logs/README.md
+++ logs/README.md
@@ -5,5 +5,21 @@ traffic sources:
  - ?campaign=friends-and-family: 5
  - ?campaign=march-for-science: 15
 
-6 people clicked on the petition
+6 of 31 people clicked on the petition
+
+12 of 31 people completed the post-survey
+
+postsurvey results
+[{'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'slightly'}, {'postsurvey.belief': 'big'}]
+[{'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'not-at-all'}, {'postsurvey.belief': 'some'}]
+[{'postsurvey.equal_or_more': 'slightly'}, {'postsurvey.how_likely': 'slightly'}, {'postsurvey.belief': 'big'}]
+[{'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'not-at-all'}, {'postsurvey.belief': 'some'}]
+[{'postsurvey.equal_or_more': 'slightly'}, {'postsurvey.how_likely': 'not-at-all'}, {'postsurvey.belief': 'some'}]
+[{'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'not-at-all'}, {'postsurvey.belief': 'some'}]
+[{'postsurvey.how_likely': 'slightly'}, {'postsurvey.belief': 'none'}]
+[{'postsurvey.belief': 'big'}, {'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'slightly'}]
+[{'postsurvey.equal_or_more': 'slightly'}, {'postsurvey.how_likely': 'slightly'}, {'postsurvey.belief': 'some'}]
+[{'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'slightly'}, {'postsurvey.belief': 'some'}]
+[{'postsurvey.how_likely': 'not-at-all'}, {'postsurvey.belief': 'some'}, {'postsurvey.equal_or_more': 'equally'}]
+[{'postsurvey.equal_or_more': 'equally'}, {'postsurvey.how_likely': 'not-at-all'}, {'postsurvey.belief': 'some'}]
 
--- logs/stats.py
+++ logs/stats.py
@@ -13,6 +13,7 @@ for file in glob("*.json"):
 report(str(len(events)) + " tracked events")
 report("")
 
+nb_people = 0
 counter = {}
 for event in events:
     href = event["context"]["location"]["href"]
@@ -25,11 +26,39 @@ for event in events:
 report("traffic sources:")
 for k, v in counter.items():
     report(" - " + k + ": " + str(len(v)))
+    nb_people += len(v)
 report("")
 
-nb_petition = 0
+petition_ip = set()
 for event in events:
     if event["data"]["event"] == "petition":
-        nb_petition += 1
-report(str(nb_petition) + " people clicked on the petition")
+        if "ip" in event["context"]["ip_info"]:
+            petition_ip.add(event["context"]["ip_info"]["ip"])
+report(str(len(petition_ip)) + " of " + str(nb_people) + " people clicked on the petition")
 report("")
+
+postsurvey_ip = {}
+for event in events:
+    if event["data"]["event"] == "postsurvey":
+        if "ip" in event["context"]["ip_info"]:
+            ip = event["context"]["ip_info"]["ip"]
+            if ip not in postsurvey_ip:
+                postsurvey_ip[ip] = []
+            del event["data"]["event"]
+            postsurvey_ip[ip].append(event["data"])
+report(str(len(postsurvey_ip)) + " of " + str(nb_people) + " people completed the post-survey")
+report("")
+
+report("postsurvey results")
+for user, values in postsurvey_ip.items():
+    report(str(values))
+report("")
+
+"""
+TODO:
+??? users completed the post-survey. ??? of these individuals (??? %) said our page made them “much more concerned” and ??? (??? %) said that 
+our page made them “slightly more concerned.” Moreover, ??? (??? %) said that they are “very likely” to change the way they commute after going 
+through our web page, compared to ??? (??? %) who said that they are “slightly likely” and ??? (???%) who said they are “not at all likely.” 
+Finally, ??? of those who filled out the post-survey (??? %) said they believe that many people greening their commutes would “make a big 
+difference,” compared to ??? (??? %) who said it would “make some difference” and ??? (???%) who said it “wouldn’t change anything.” 
+"""
