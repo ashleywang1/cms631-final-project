eee9afd98f48d87935b0be4ff9dbe95606cf6cc9
Kevin Zhang <kevz@mit.edu>
2017-05-17 17:07:34 -0400

    Added 6

--- logs/stats.py
+++ logs/stats.py
@@ -3,4 +3,16 @@ from glob import glob
 
 events = []
 for file in glob("*.json"):
-    events.extend(load(open(file, "rt")))
+    events.extend(load(open(file, "rt", encoding="utf8")))
+print(len(events), "events")
+
+counter = {}
+for event in events:
+    href = event["context"]["location"]["href"]
+    if "?" in href:
+        href = href[href.index("?"):]
+        if href not in counter:
+            counter[href] = set()
+        if "ip" in event["context"]["ip_info"]:
+            counter[href].add(event["context"]["ip_info"]["ip"])
+print({k: len(v) for k, v in counter.items()})
