1ae7513576b297033e6264fe5913d98b6a494f22
Kevin Zhang <kevz@mit.edu>
2017-05-17 17:17:17 -0400

    Added stats

---
+++ logs/README.md
@@ -0,0 +1,9 @@
+292 tracked events
+
+traffic sources:
+    ?campaign=friends-and-family: 5
+    ?campaign=reddit: 11
+    ?campaign=march-for-science: 15
+
+6 people clicked on the petition
+
--- logs/stats.py
+++ logs/stats.py
@@ -1,10 +1,17 @@
 from json import load
 from glob import glob
 
+fout = open("README.md", "wt")
+def report(text):
+    print(text)
+    fout.write(text + "\n")
+
+
 events = []
 for file in glob("*.json"):
     events.extend(load(open(file, "rt", encoding="utf8")))
-print(len(events), "events")
+report(str(len(events)) + " tracked events")
+report("")
 
 counter = {}
 for event in events:
@@ -15,4 +22,14 @@ for event in events:
             counter[href] = set()
         if "ip" in event["context"]["ip_info"]:
             counter[href].add(event["context"]["ip_info"]["ip"])
-print({k: len(v) for k, v in counter.items()})
+report("traffic sources:")
+for k, v in counter.items():
+    report("    " + k + ": " + str(len(v)))
+report("")
+
+nb_petition = 0
+for event in events:
+    if event["data"]["event"] == "petition":
+        nb_petition += 1
+report(str(nb_petition) + " people clicked on the petition")
+report("")
